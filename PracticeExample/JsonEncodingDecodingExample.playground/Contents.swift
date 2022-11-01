dab28123384b774c500f4b0c807c6e54fb5fe823
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-11-01 23:01:06 +0600

    advance codable practice added

--- PracticeExample/JsonEncodingDecodingExample.playground/Contents.swift
+++ PracticeExample/JsonEncodingDecodingExample.playground/Contents.swift
@@ -29,4 +29,65 @@ if let path = Bundle.main.path(forResource: "Data", ofType: "json"),
         
         // Encoding successful! (json)
         dump(String(data: encodedUser, encoding: .utf8))
+    }
+
+
+
+// Advance Encoding decoding.....
+struct AdvanceUser: Codable {
+    let Id: Int
+    let userName: String
+    let nickName: String
+    let occupation: Occupation?
+    let dateOfBirth: Date
+    
+    // CodingKey for changing variable name different form json
+    enum CodingKeys: String, CodingKey {
+        case Id = "userId"
+        case userName
+        case nickName = "nick_name"
+        case occupation
+        case dateOfBirth
+    }
 }
+
+struct Occupation: Codable {
+    let name: String
+    let isActive: Bool
+}
+
+
+if let advancePath = Bundle.main.path(forResource: "AdvanceData", ofType: "json"),
+   let advanceData = FileManager.default.contents(atPath: advancePath) {
+    
+    // Decoding
+    let advanceDecoder = JSONDecoder()
+    
+    advanceDecoder.dateDecodingStrategy = .iso8601 // convert string to date format
+    
+    let advanceUser = try advanceDecoder.decode([AdvanceUser].self, from: advanceData)
+    dump(advanceUser)
+    
+    // Loop through all users
+    for user in advanceUser {
+        print("UserId:", user.Id)
+        print("UserName:", user.userName)
+        print("NickName:", user.nickName)
+        print("Occupation Name:", user.occupation?.name ?? "Unemployed")
+        print("Occupation Status:", user.occupation?.isActive ?? "Nothing")
+        print("Date of Birth:", user.dateOfBirth)
+    }
+        
+    // Encoding
+    let advanceEncoder = JSONEncoder()
+    
+    advanceEncoder.dateEncodingStrategy = .iso8601 // string to date conversion
+    
+    // Automaticly convert camelCase to snakeCase
+    advanceEncoder.keyEncodingStrategy = .convertToSnakeCase
+    
+    let advanceEncodedUser = try advanceEncoder.encode(advanceUser)
+    dump(String(data: advanceEncodedUser, encoding: .utf8))
+    
+}
+
